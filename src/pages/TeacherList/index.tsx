07893fb4d217e19240e79854beb6bfffac6fecb4
pablo <pablo@radius.net.br>
2020-08-06 19:52:24 -0300

    melhorias no css e tornando labels fixas em componentes

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -6,14 +6,41 @@ import Input from '../../components/Input';
 
 // import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 import './styles.css'
+import Select from '../../components/Select';
 
 function TeacherList() {
     return (
       <div id="page-teacher-list" className="container">
         <PageHeader title="Estes são os proffys disponíveis.">
           <form id="search-teachers">
-           <Input name="subject" label="Matéria"/>
-           <Input name="week" label="Dia da semana"/>
+
+          <Select 
+              name="subject" 
+              label="Matéria"
+              options={[
+                  { value: 'Artes', label: 'Artes'},
+                  { value: 'Biologia', label: 'Biologia'},
+                  { value: 'Ciencia', label: 'Ciencia'},
+                  { value: 'Ed. Fisica', label: 'Ed. Fisica'},
+                  { value: 'fisica', label: 'fisica'},
+                  { value: 'Historia', label: 'Historia'},
+              ]}
+          />
+
+          <Select 
+              name="week" 
+              label="Dia da semana"
+              options={[
+                  { value: '0', label: 'Domingo'},
+                  { value: '1', label: 'Segunda-Feira'},
+                  { value: '2', label: 'Terça-Feira'},
+                  { value: '3', label: 'Quarta-Feria'},
+                  { value: '4', label: 'Quinta-Feira'},
+                  { value: '5', label: 'Sexta-Feira'},
+                  { value: '6', label: 'Sabado'},
+              ]}
+          />
+           {/* <Input name="week" label="Dia da semana"/> */}
            <Input type="time" name="time" label="Hora"/>
           </form>
         </PageHeader>
